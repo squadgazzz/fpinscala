7e2e0c5e2d7633440fb3f5fa0b4b3b9db25bbe39
i.zhavoronkov <ilya.zhavoronkov@stoloto.ru>
2019-05-16 15:34:53 +0300

    lists finished with *

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -170,4 +170,17 @@ object List { // `List` companion object. Contains functions for creating and wo
       case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
     }
 
+  @annotation.tailrec
+  def startsWith[A](l: List[A], prefix: List[A]): Boolean = (l, prefix) match {
+    case (_, Nil)                              => true
+    case (Cons(h, t), Cons(h2, t2)) if h == h2 => startsWith(t, t2)
+    case _                                     => false
+  }
+
+  @annotation.tailrec
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sup match {
+    case Nil                       => sub == Nil
+    case _ if startsWith(sup, sub) => true
+    case Cons(h, t)                => hasSubsequence(t, sub)
+  }
 }
