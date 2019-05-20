e085a66f19f99231ebc2ff892541f7dd0dd095b1
i.zhavoronkov <ilya.zhavoronkov@stoloto.ru>
2019-05-20 15:26:21 +0300

    tree + option

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -39,6 +39,7 @@ object Tree {
         case Leaf(v) :: xs                  => go(xs, v max acc)
         case Branch(l, r) :: xs             => go(l :: r :: xs, acc)
       }
+
     go(scala.collection.immutable.List(t), 0)
   }
 
@@ -47,9 +48,30 @@ object Tree {
     case Branch(l, r) => 1 + (depth(l) max depth(r))
   }
 
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v)      => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(v)      => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def sizeWithFold[A](t: Tree[A]): Int = fold(t)(_ => 1)(1 + _ + _)
+
+  def maxWithFold(t: Tree[Int]): Int = fold(t)(a => a)(_ max _)
+
+  def depthWithFold[A](t: Tree[A]): Int = fold(t)(_ => 0)((d1, d2) => 1 + d1 max d2)
+
   def main(args: Array[String]): Unit = {
+    val l = scala.collection.immutable.List(1,2,3)
     val tree = Branch(Branch(Leaf(10), Leaf(20)), Branch(Leaf(90), Leaf(1)))
     println(size(tree))
+    println(sizeWithFold(tree))
     println(max(tree))
+    println(maxWithFold(tree))
+    println(depth(tree))
+    println(depthWithFold(tree))
   }
 }
